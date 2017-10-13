7e8fadaa8d4f53d12959d1d990c30fc0d2b93992
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-10-13 15:53:09 -0300

    foi implementado a comunicação com a api

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -4,12 +4,7 @@
   <div>
     <h1>{{ titulo }}</h1>
 
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
-        {{foto.url }} - {{ foto.titulo }}
-      </li>
-    </ul>
+    <img v-for="foto of fotos" :src="foto.url" :alt="foto.titulo">
 
   </div>
 </template>
@@ -19,20 +14,18 @@ export default {
   data() {
     return {
       titulo: 'Alurapic',
-      fotos: [
-        {
-          url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
-          titulo: 'Cachorro'
-        },
-        {
-          url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOhmlmzV4-Sifx5BIc2SXeA-1CtZJf8jb8V_vPZyKbXIQJKU-rkxGO6OM',
-          titulo: 'Gato'
-        }
-      ]
+      fotos: []
     }
+  },
+  created() {
+    // buscaremos nossas fotos aqui
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
   }
 }
 </script>
 
 <style>
+
 </style>
