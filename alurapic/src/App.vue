c82fce187951c0e627c338e1e1a7ec21216ed7cb
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-10-14 11:23:45 -0300

    foi adicionado mais alguns estilo no painel

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -6,7 +6,13 @@
 
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+
+        <div class="painel">
+          <h2 class="painel-titulo">{{foto.titulo}}</h2>
+          <div class="painel-corpo">
+            <img :src="foto.url" :alt="foto.titulo">
+          </div>
+        </div>
       </li>
     </ul>
 
@@ -41,5 +47,28 @@ export default {
 
 .lista-fotos-item {
   display: inline-block;
+}
+
+/* estilo do painel */
+
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
 }
 </style>
