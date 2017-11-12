704c60a1574e86080cd478264d8644c6b361066b
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-11-12 11:55:25 -0300

    foi criado um componente para as imgens

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -6,7 +6,7 @@
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
-          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
         </meu-painel>
       </li>
     </ul>
@@ -15,24 +15,26 @@
 
 <script>
 import Painel from "./components/shared/painel/Painel.vue";
+import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue'
 
 export default {
   components: {
-    "meu-painel": Painel
+    "meu-painel": Painel,
+    "imagem-responsiva": ImagemResponsiva
   },
 
   data() {
     return {
-      titulo: 'Alurapic',
+      titulo: "Alurapic",
       fotos: [],
-      filtro: ''
+      filtro: ""
     };
   },
 
   computed: {
     fotosComFiltro() {
       if (this.filtro) {
-        let exp = new RegExp(this.filtro.trim(), 'i');
+        let exp = new RegExp(this.filtro.trim(), "i");
         return this.fotos.filter(foto => exp.test(foto.titulo));
       } else {
         return this.fotos;
@@ -66,10 +68,6 @@ export default {
 
 .lista-fotos .lista-fotos-item {
   display: inline-block;
-}
-
-.imagem-responsiva {
-  width: 100%;
 }
 
 .filtro {
