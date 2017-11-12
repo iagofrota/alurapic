125da7cca89b794042f006c6ca65b647cda5448a
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-11-12 11:12:21 -0300

    adicionando compatamento ao componente Paniel.vue

--- alurapic/src/components/shared/painel/Painel.vue
+++ alurapic/src/components/shared/painel/Painel.vue
@@ -1,42 +1,47 @@
-
 <template>
     <div class="painel">
-        <h2 class="painel-titulo">{{ titulo }}</h2>
-        <slot class="painel-conteudo">
-        </slot>
+        <h2 class="painel-titulo" @dblclick="visivel = !visivel">{{ titulo }}</h2>
+        <div class="painel-conteudo" v-show="visivel">
+            <slot >
+            </slot>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['titulo']
-}
-
+  props: ["titulo"],
+  data() {
+    return {
+      visivel: true
+    };
+  }
+};
 </script>
 
 <style scoped>
 .painel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
 }
 
 .painel .painel-titulo {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
 }
 
 * {
-    box-shadow: 5px 5px 5px;
+  box-shadow: 5px 5px 5px;
 }
 </style>
