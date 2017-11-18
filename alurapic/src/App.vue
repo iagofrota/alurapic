ed134cd754b11a94209fcc22ccea782012004420
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-11-18 18:29:44 -0300

    animated page transition

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,16 +1,17 @@
-
 <template>
   <div class="corpo">
-    <nav>
-      <ul>
-        <li v-for="route in routes">
-          <router-link :to="route.path ? route.path : '/'">
-            {{route.titulo}}
-          </router-link>
-        </li>
-      </ul>
-    </nav>
-    <router-view></router-view>
+	<nav>
+	  <ul>
+		<li v-for="route in routes">
+		  <router-link :to="route.path ? route.path : '/'">
+			{{route.titulo}}
+		  </router-link>
+		</li>
+	  </ul>
+	</nav>
+	<transition name="pagina">
+		<router-view></router-view>
+	</transition>
   </div>
 </template>
 
@@ -31,5 +32,14 @@ export default {
   font-family: Helvetica, sans-serif;
   width: 96%;
   margin: 0 auto;
+}
+
+.pagina-enter-active,
+.pagina-leave-active {
+  transition: opacity 0.3s;
+}
+.pagina-enter,
+.pagina-leave-active {
+  opacity: 0;
 }
 </style>
