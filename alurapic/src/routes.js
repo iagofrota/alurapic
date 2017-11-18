be6e77e6542e9d0898da10d4e17c7acd58d4216c
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-11-18 12:10:44 -0300

    the application menu

--- alurapic/src/routes.js
+++ alurapic/src/routes.js
@@ -2,6 +2,14 @@ import Home from './components/home/Home.vue';
 import Cadastro from './components/cadastro/Cadastro.vue';
 
 export const routes = [
-    { path: '', component: Home },
-    { path: '/cadastro', component: Cadastro }
+    {
+        path: '',
+        component: Home,
+        titulo: 'Home'
+    },
+    {
+        path: '/cadastro',
+        component: Cadastro,
+        titulo: 'Cadastro'
+    }
 ];
