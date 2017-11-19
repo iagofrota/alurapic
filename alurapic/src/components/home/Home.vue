d328df2f8864f94a5b34154e93e00d38862bfef0
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-11-19 11:12:34 -0300

    created component of varied usage

--- alurapic/src/components/home/Home.vue
+++ alurapic/src/components/home/Home.vue
@@ -6,6 +6,7 @@
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
           <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+          <meu-botao tipo="button" rotulo="REMOVER" />
         </meu-painel>
       </li>
     </ul>
@@ -15,11 +16,13 @@
 <script>
 import Painel from "../shared/painel/Painel.vue";
 import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
+import Botao from '../shared/botao/Botao.vue';
 
 export default {
   components: {
     "meu-painel": Painel,
-    "imagem-responsiva": ImagemResponsiva
+    "imagem-responsiva": ImagemResponsiva,
+    "meu-botao": Botao
   },
 
   data() {
