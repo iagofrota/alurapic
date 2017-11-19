124e96a5b8e354a6ee619924d46740ff1b7a1fc6
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-11-19 11:26:09 -0300

    methods remove() created

--- alurapic/src/components/home/Home.vue
+++ alurapic/src/components/home/Home.vue
@@ -6,7 +6,7 @@
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
           <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
-          <meu-botao tipo="button" rotulo="REMOVER" />
+          <meu-botao tipo="button" rotulo="REMOVER" @click.native="remove(foto)"/>
         </meu-painel>
       </li>
     </ul>
@@ -15,8 +15,8 @@
 
 <script>
 import Painel from "../shared/painel/Painel.vue";
-import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
-import Botao from '../shared/botao/Botao.vue';
+import ImagemResponsiva from "../shared/imagem-responsiva/ImagemResponsiva.vue";
+import Botao from "../shared/botao/Botao.vue";
 
 export default {
   components: {
@@ -31,6 +31,14 @@ export default {
       fotos: [],
       filtro: ""
     };
+  },
+
+  methods: {
+    remove(foto) {
+      if (confirm("Confirma ?")) {
+        alert(foto.titulo);
+      }
+    }
   },
 
   computed: {
